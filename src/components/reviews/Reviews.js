69f398d3c791b42ef81163a5f1ab18cac36cd252
Wurdsmith <wurdsmithing@gmail.com>
2021-06-01 05:50:51 -0500

    commit tree 0d6e4b0c96a4506e6372a1319570cd72b690e7a7

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const matchedReviews = reviews.filter(review => review.restaurantId === restaurantId);
+    
+    const restaurantReviews = matchedReviews.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={deleteReview} />
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {restaurantReviews}
+        </ul>
+      </div>
     );
   }
+
 };
 
 export default Reviews;
